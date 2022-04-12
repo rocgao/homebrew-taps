b36c0ef439bdb8a771c4b3892dc75d6ad4effbc3
高鹏 <gaopeng07@meituan.com>
2022-04-12 14:32:21 +0800

    修复rlive.rb配置错误

--- Formula/rlive.rb
+++ Formula/rlive.rb
@@ -11,11 +11,7 @@ class Rlive < Formula
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "rlive"
   end
 
   test do
